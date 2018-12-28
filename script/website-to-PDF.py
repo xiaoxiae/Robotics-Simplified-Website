441b410e00189e1c80f3241972699231a778d0b3
xiaoxiae <tomas.slama.131@gmail.com>
2018-12-28 17:23:23 +0100

    add conversion logic
    Add the conversion logic, so the articles are in the correct order.

--- script/website-to-PDF.py
+++ script/website-to-PDF.py
@@ -26,27 +26,65 @@ subRegex = [
     ("{:.+?}", "")                                      # delete {:...} lines
 ]
 
+# the groups of converted files
+convertedGroups = {}
+
 # read through all of the files
+
 for path, subdirs, files in os.walk("../docs/"):
+    #  contents of the converted files
+    convertedFiles = {}
+
     for name in files:
-        fileName = os.path.join(path, name)
-        fileContents = open(fileName, 'r').read()
-        fileOrder = search("nav_order: ([0-9]+)", fileContents).group(1)
+        filePath = os.path.join(path, name)
+        fileContents = open(filePath, 'r').read()
+        fileOrder = int(search("nav_order: ([0-9]+)", fileContents).group(1))
+
+        # if the file is in the form of \topic\topic.md - the main topic file
+        isMainTopic = search("(.+)"+os.sep+"\\\\1\.", filePath) != None
+
+        # the "depth" of the file - how many levels of folders it is in
+        fileDepth = filePath[8:].count(os.sep)
 
         # run the file through the substitution regexes
         for regex in subRegex:
             fileContents = sub(*regex, fileContents, flags=MULTILINE)
 
+        # replace the headings based on the file depth - the further it is in,
+        # the more sub will be added in in front of section{...}
+        fileContents = sub("^(#+) (.+)",\
+            lambda x: "\\"\
+                + (len(x.group(1)) + fileDepth - 1 - int(isMainTopic)) * "sub"\
+                + "section{" + x.group(2)\
+                + "}",\
+            fileContents, flags=MULTILINE)
+
+        # if it's at depth 0 by default - files like about.md, preface.md...,
+        # convertedGroups[fileOrder] will be the file itself
+        if fileDepth == 0:
+            convertedGroups[fileOrder] = fileContents
+
+        # if it's at depth 0 because it is the main topic,
+        # convertedGroups[fileOrder] will be the group of files
+        elif fileDepth - int(isMainTopic) == 0:
+            convertedGroups[fileOrder] = convertedFiles
+
+        # add the converted file to the fileOrder position
+        convertedFiles[fileOrder if not isMainTopic else 0] = fileContents
+
 
 output = open("output.tex", "w")
 
-# we have to add \n characters to lines manually, because writelines() doesn't
-nl = lambda x:x+"\n"
+# write beginning
+output.write(open("beginning", "r").read())
 
-# write the beginning
-beginning = list(map(nl, open("beginning", "r").read().splitlines()))
-output.writelines(beginning)
+# write the converted articles in their respective order
+for k1, v1 in sorted(convertedGroups.items()):
+    if type(v1) == str:
+        output.write(v1 + "\n")
+    else:
+        for k2, v2 in sorted(v1.items()):
+            output.write(v2 + "\n")
 
-# write the end
-ending = list(map(nl, open("ending", "r").read().splitlines()))
-output.writelines(ending)
+# write ending
+output.write(open("ending", "r").read())
