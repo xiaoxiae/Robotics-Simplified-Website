19f8aa9b17f366835a85e8993dc04afe8bc2e612
xiaoxiae <tomas.slama.131@gmail.com>
2019-01-13 23:46:36 +0100

    refactor generate.py
    Import only the required os functions and classes, get rid of the ugly os.sep everywhere, update comments.

--- script/generate.py
+++ script/generate.py
@@ -1,9 +1,9 @@
 from regex import sub, match, search, MULTILINE
 from datetime import datetime
-import os
+from os import walk, path, sep, makedirs
 
 # regular expressions and their substitution results
-subRegex = [
+substitutions = [
     ("---\n(.+\n)+---", ""),                            # remove config
     ("^---\n", ""),                                     # remove linebreaks
     ("^Modified.+", ""),                                # remove last modified
@@ -14,7 +14,7 @@ subRegex = [
     ("\*(.+?)\*", "\\\\textit{\g<1>}"),                 # * italics
     ("(```python\n){% +include +(.+?) +%}(\n```)",\
         lambda x: x.group(1) + \
-        open(".."+os.sep+"_includes"+os.sep+x.group(2), "r").read() + \
+        open(path.join("..", "_includes", x.group(2)), "r").read() + \
         x.group(3)),                                    # insert code snippet
     ("```python(\n(.*\n)+?)```",
      "\\\\begin{lstlisting}\g<1>\\\\end{lstlisting}"),  # code highlights
@@ -38,21 +38,20 @@ subRegex = [
 convertedGroups = {}
 
 # read through all of the files
-
-for path, subdirs, files in os.walk(".." + os.sep + "docs" + os.sep):
+for root, subdirs, files in walk(path.join("..", "docs")):
     #  contents of the converted files
     convertedFiles = {}
 
     for name in files:
-        fPath = os.path.join(path, name)
+        fPath = path.join(root, name)
         fContent = open(fPath, 'r').read()
         fileOrder = int(search("nav_order: ([0-9]+)", fContent).group(1))
 
-        # if the file is in the form of \topic\topic.md - the main topic file
+        # if the file is in the form of .../topic/topic.md - the main topic file
         isMainTopic = search(r"(.+)[\\\/]\1\.", fPath) != None
 
         # the "depth" of the file - how many levels of folders it is in
-        fDepth = fPath[8:].count(os.sep)
+        fDepth = fPath[8:].count(sep)
 
         # replace the headings based on the file depth - the further it is in,
         # the more sub will be added in in front of section{...}
@@ -64,8 +63,8 @@ for path, subdirs, files in os.walk(".." + os.sep + "docs" + os.sep):
             fContent, flags=MULTILINE)
 
         # run the file through the substitution regexes
-        for regex in subRegex:
-            fContent = sub(*regex, fContent, flags=MULTILINE)
+        for substitution in substitutions:
+            fContent = sub(*substitution, fContent, flags=MULTILINE)
 
         # if it's at depth 0 by default - files like about.md, preface.md...,
         # convertedGroups[fileOrder] will be the file itself
@@ -85,7 +84,7 @@ for path, subdirs, files in os.walk(".." + os.sep + "docs" + os.sep):
 latexOut = open("website.tex", "w")
 
 # write beginning
-latexOut.write(open("tex-generation-files" + os.sep + "beginning", "r").read())
+latexOut.write(open(path.join("tex-generation-files", "beginning"), "r").read())
 
 # write the converted articles in their respective order
 for k1, v1 in sorted(convertedGroups.items()):
@@ -96,19 +95,20 @@ for k1, v1 in sorted(convertedGroups.items()):
             latexOut.write(v2[0] + "\n")
 
 # write ending
-latexOut.write(open("tex-generation-files" + os.sep + "ending", "r").read())
+latexOut.write(open(path.join("tex-generation-files", "ending"), "r").read())
 
 
-def writeURL(url, subpath, xmlFile, priority, lastmod = None):
+def writeURL(url, subpath, xmlFile, priority = 0.8, lastmod = None):
     """Writes information about a specific URL to the sitemap.xml file."""
     address = url + "/" + subpath.replace(".md", "/")[8:].replace("\\", "/")
 
     # if lastmod wasn't specified, generate it from the path
     if lastmod == None:
         lastmod = (datetime\
-            .utcfromtimestamp(os.path.getmtime(subpath))\
+            .utcfromtimestamp(path.getmtime(subpath))\
             .strftime('%Y-%m-%dT%H:%M:%S+00:00'))
 
+    # the contents of the xml record, in a list
     contents = ["<url>",
                 "\t<loc>" + address + "</loc>",
                 "\t<lastmod>" + lastmod + "</lastmod>",
@@ -120,35 +120,34 @@ def writeURL(url, subpath, xmlFile, priority, lastmod = None):
 
 
 # create folder where sitemap should be
-if not os.path.exists(".." + os.sep + "_site" + os.sep):
-    os.makedirs(".." + os.sep + "_site" + os.sep)
+if not path.exists(path.join("..", "_site")):
+    makedirs(path.join("..", "_site"))
 
 # the generated sitemap
-sitemapOut = open(".." + os.sep + "_site" + os.sep + "sitemap.xml", "w")
+sitemapOut = open(path.join("..", "_site", "sitemap.xml"), "w")
 
 # write beginning
-sitemapOut.write(open("xml-generation-files" + os.sep + "beginning", "r").read())
+sitemapOut.write(open(path.join("xml-generation-files", "beginning"), "r").read())
 
 # get the url of the website from _config.yml
 url = search("^url: \"(.+)\".*\n",\
-    open(".." + os.sep + "_config.yml", "r").read(),\
-    MULTILINE).group(1)
+    open(path.join("..", "_config.yml"), "r").read(), MULTILINE).group(1)
 
 # write information about the main page
 writeURL(url, "", sitemapOut, "1.00",\
-    datetime.utcfromtimestamp(os.path.getmtime("../index.md"))\
+    datetime.utcfromtimestamp(path.getmtime(path.join("..", "index.md")))\
             .strftime('%Y-%m-%dT%H:%M:%S+00:00'))
 
 # write all of the URLs to the .xml file
 for k1, v1 in sorted(convertedGroups.items()):
     if type(v1[1]) == str:
-        writeURL(url, v1[1], sitemapOut, "0.80")
+        writeURL(url, v1[1], sitemapOut)
     else:
         for k2, v2 in sorted(v1.items()):
-            writeURL(url, v2[1], sitemapOut, "0.80")
+            writeURL(url, v2[1], sitemapOut)
 
 # write ending
-sitemapOut.write(open("xml-generation-files" + os.sep + "ending", "r").read())
+sitemapOut.write(open(path.join("xml-generation-files", "ending"), "r").read())
 
 # properly close the files
 latexOut.close()
