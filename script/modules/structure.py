ea498fc8ccd6b119590f006642be6f99f21a2791
Unknown <tomas.slama.131@gmail.com>
2019-01-22 14:54:35 +0100

    fix structure.py
    The function wasn't working if the script that was calling it was in a separate folder. This fixes the issue by

--- script/modules/structure.py
+++ script/modules/structure.py
@@ -9,7 +9,7 @@ def _build_structure(structure_dict, structure_list):
     for k, v in sorted(structure_dict.items()):
         # either add the path to file to the list or call itself recursively
         if type(v) == str:
-            structure_list.append(sub(r"(.+?docs[\\\/])", "", v))
+            structure_list.append(v)
         elif type(v) == dict:
             _build_structure(v, structure_list)
 
@@ -21,7 +21,8 @@ def get_docs_structure():
     structure_dict = {}
 
     # read through all files in the current folder and all its subfolders
-    for root, subdirs, files in walk(path.join("..", "..", "docs")):
+    for root, subdirs, files in walk(\
+        path.join(path.dirname(path.realpath(__file__)), "..", "..", "docs")):
         # for ordering topic articles
         topic_structure = {}
 
@@ -31,8 +32,11 @@ def get_docs_structure():
             file_content = open(file_path, 'r').read()
             file_order = int(search("nav_order: ([0-9]+)", file_content).group(1))
 
+            # remove everything before (and including) docs/
+            file_path = sub(r"(.+?\.\.[\\\/]docs[\\\/])", "", file_path)
+
             # if it's a file directly in the docs/ directory
-            if file_path.count(sep) == 3:
+            if file_path.count(sep) == 0:
                 structure_dict[file_order] = file_path
 
             # if it's the main topic article
